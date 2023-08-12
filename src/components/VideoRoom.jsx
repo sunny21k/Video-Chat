f23762259d150e595583011ee7a6321dd91edba0
Sunny Kumar <sunny9102003kumar@gmail.com>
2023-08-12 14:25:43 -0400

    videochat

---
+++ src/components/VideoRoom.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState, useRef } from 'react';
+import AgoraRTC from 'agora-rtc-sdk-ng';
+import { VideoPlayer } from './VideoPlayer';
+
+const APP_ID = 'e6cfb49f752d40258585ae8b66b48080';
+const TOKEN =
+  '007eJxTYFjWa8P5/lb1m6DFsyMXsNz6uOj4vuNnd5qkMB3M2vWRgz9KgSHVLDktycQyzdzUKMXEwMjUAggTUy2SzMySTCwMLAzKZPakNAQyMojN2snKyACBID4LQ3JGYgkDAwDUxiB0';
+const CHANNEL = 'chat';
+
+const client = AgoraRTC.createClient({
+  mode: 'rtc',
+  codec: 'vp8',
+});
+
+export const VideoRoom = () => {
+  const [users, setUsers] = useState([]);
+  const [localTracks, setLocalTracks] = useState([]);
+  const tracksRef = useRef([]);
+
+  const handleUserJoined = async (user, mediaType) => {
+    await client.subscribe(user, mediaType);
+
+    if (mediaType === 'video') {
+      setUsers((previousUsers) => [...previousUsers, user]);
+    }
+
+    if (mediaType === 'audio') {
+      // user.audioTrack.play()
+    }
+  };
+
+  const handleUserLeft = (user) => {
+    setUsers((previousUsers) =>
+      previousUsers.filter((u) => u.uid !== user.uid)
+    );
+  };
+
+  useEffect(() => {
+    client.on('user-published', handleUserJoined);
+    client.on('user-left', handleUserLeft);
+
+    client
+      .join(APP_ID, CHANNEL, TOKEN, null)
+      .then((uid) =>
+        Promise.all([
+          AgoraRTC.createMicrophoneAndCameraTracks(),
+          uid,
+        ])
+      )
+      .then(([tracks, uid]) => {
+        const [audioTrack, videoTrack] = tracks;
+        tracksRef.current = tracks;
+        setLocalTracks(tracks);
+        setUsers((previousUsers) => [
+          ...previousUsers,
+          {
+            uid,
+            videoTrack,
+            audioTrack,
+          },
+        ]);
+        client.publish(tracks);
+      });
+
+    return () => {
+      for (let localTrack of tracksRef.current) {
+        localTrack.stop();
+        localTrack.close();
+      }
+      client.off('user-published', handleUserJoined);
+      client.off('user-left', handleUserLeft);
+      client.unpublish(tracksRef.current).then(() => client.leave());
+    };
+  }, []);
+
+  return (
+    <div
+      style={{ display: 'flex', justifyContent: 'center' }}
+    >
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(2, 200px)',
+        }}
+      >
+        {users.map((user) => (
+          <VideoPlayer key={user.uid} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+};
